03072cb47e4a55ff65f1c596ded19b65d68a1cb7
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2020-04-07 17:21:54 +0530

    Added code for parallelization

--- Main.py
+++ Main.py
@@ -11,25 +11,24 @@ if __name__ == "__main__" :
         'k0'    : [1/7], 
         'kt'    : [0.075],
         'mu'    : [1/7],
-        'sigma' : np.arange(0, 0.5, 0.05),
+        'sigma' : np.arange(0, 0.5, 0.25),
         'gamma1': [1/19],
         'gamma2': [1/14],
         'gamma3': [1/14],
         'N'     : [1.1e8],
-        'beta'  : np.arange(0, 1, 0.05),
-        'beta1' : np.arange(0, 1, 0.05)
+        'beta'  : np.arange(0, 1, 0.25),
+        'beta1' : np.arange(0, 1, 0.25)
     }
-    I0  = np.arange(1, 5000, 100)
-    A0  = np.arange(0, 5000, 100)
-    S0  = list(map(lambda x : 1.1e8 - x[0] - x[1], product(I0, A0)))
+    I0  = np.arange(1, 5000, 1000)
+    A0  = np.arange(0, 5000, 1000)
     Xs0 = [0]
     Xa0 = [0]
     Xi0 = [0]
     P0  = [0]
     R0  = [0]
-    ivRanges = [S0, A0, I0, Xs0, Xa0, Xi0, P0, R0]
+    ivRanges = [A0, I0, Xs0, Xa0, Xi0, P0, R0]
     data = pandas.read_csv('./Data/maha_data.csv')
     lastDateIdx = data[data['Date'] == '31 Mar'].index[0]
     data = data.iloc[:lastDateIdx]
-    result = gridSearch(ivRanges, paramRanges, data, squaredLoss, np.arange(3, 31) - 3)
+    result = parallelGridSearch(ivRanges, paramRanges, data, squaredLoss, np.arange(3, 31) - 3)
     print(result)
--- Search.py
+++ Search.py
@@ -3,6 +3,7 @@ from functools import partial
 from Simulate import *
 from Model import * 
 from Search import *
+from multiprocessing import Process, Pipe, cpu_count
 import math
 
 def gridSearch (ivRanges, paramRanges, groundTruth, lossFunction, T) :
@@ -29,3 +30,102 @@ def gridSearch (ivRanges, paramRanges, groundTruth, lossFunction, T) :
 
     return minx0, minParams
 
+def worker (conn, groundTruth, lossFunction, T) :
+
+    # Do pre-processing of the deaths.
+    lo, hi = T.min(), T.max()
+    samples = 5
+    T_ = np.linspace(lo, hi, (hi - lo) * samples)
+    startIdx = groundTruth[groundTruth['Date'] == '20 Mar'].index[0] 
+    deaths = groundTruth['New Deaths'][startIdx:].to_numpy()
+    nDays = deaths.size
+
+    while True : 
+        item = conn.recv() 
+        if item == b'e' : 
+            conn.send(b'e')
+            break
+        else : 
+            point, minLoss = item
+            x0, params = point
+            N = params['N']
+            x0 = [N - sum(x0), *x0]
+            model = Sixer(x0, params)
+            result = simulator(model, T_)
+            infections = result[:, 2][::samples]
+            tested = result[:, -1][::samples]
+            deathEstimate = 0.02 * (infections + tested)
+            deathEstimate = deathEstimate[:nDays]
+            loss = lossFunction(deaths, deathEstimate)
+            if loss < minLoss: 
+                conn.send((point, loss))
+            else : 
+                conn.send(b'n')
+
+    conn.close()
+
+def parallelGridSearch (ivRanges, paramRanges, groundTruth, lossFunction, T) :
+    totalChildren = cpu_count()
+
+    # Setup duplex connections between parent and child.
+    connections = [Pipe() for _ in range(totalChildren)]
+
+    # Setup Child Processes
+    processes = [
+        Process(target=worker, args=(conn, groundTruth, lossFunction, T))
+        for _, conn in connections
+    ]  
+
+    # Start Child Processes
+    for p in processes : 
+        p.start() 
+
+    doneChildren =  0
+
+    # Grid point generator
+    points = product(product(*ivRanges), dictProduct(paramRanges)) 
+
+    # Best grid point
+    minLoss = math.inf
+    minX0, minParams = None, None
+
+    # Send initial data
+    for p, _ in connections : 
+        p.send((next(points), minLoss))
+
+    while doneChildren < totalChildren : 
+
+        # Poll all connections and 
+        # send stuff if they are available.
+        for conn, _ in connections : 
+            hasSomething = conn.poll()
+            if hasSomething : 
+                obj = conn.recv() 
+                if obj == b'e' :
+                    # This signals that the child
+                    # is done.
+                    doneChildren += 1
+                elif obj != b'n' : 
+                    # This signals that the child
+                    # found an improving grid point.
+                    point, loss = obj
+                    if loss < minLoss : 
+                        minX0, minParams = point
+                        minLoss = loss
+
+            # Sentinel to guard against the 
+            # possibility of querying an empty generator.
+            point = next(points, False)
+
+            if point : 
+                conn.send((point, minLoss))
+            else : 
+                # If no more points are left, signal
+                # to the child.
+                conn.send(b'e')
+
+    # Join all child processes.
+    for p in processes : 
+        p.join()
+
+    return minX0, minParams, minLoss
