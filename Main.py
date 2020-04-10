463fcdb94c47980d5006011c48a34097025fe523
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2020-04-10 13:14:41 +0530

    Some Shit is happening

--- Main.py
+++ Main.py
@@ -8,47 +8,58 @@ import pandas
 
 def getModel () : 
     params = {
-        'tl'    : 21, 
-        'te'    : 42,
+        'tl'    : 10, 
+        'te'    : 20,
         'k0'    : 1/7, 
         'kt'    : 0.075,
         'mu'    : 1/7,
-        'sigma' : 1/38,
+        'sigma' : 1/5,
         'gamma1': 1/21,
         'gamma2': 1/21,
         'gamma3': 1/17,
         'N'     : 1.1e8,
-        'beta'  : 0.2,
-        'beta1' : 0.1,
-        'beta2' : 0.1
+        'beta'  : 0.16,
+        'beta1' : 1.8,
+        'beta2' : 0.1,
         'f'     : 0.1
     }
     return Spaxire(params)
 
 if __name__ == "__main__" : 
-    startDate = '29 Feb'
-    N   = 1e8
-    A0, I0 = 1, 1
-    init = np.array([N - A0 - I0, A0, I0, 0, 0, 0, 0, 0])
+    def pltColumn (idx) : 
+        dstd = np.sqrt(np.array([np.diag(P)[idx] for P in Ps_]))
+        d   = np.array([x[idx] for x in xs])
+        d_  = np.array([x[idx] for x in xs_])
+        
+        print(dstd)
+        x = np.arange(days)
+        plt.plot(x, d , c='red' , label=f'True {names[idx]}')
+        plt.plot(x, d_, c='blue', label=f'Estimate {names[idx]}')
+        plt.fill_between(x, d_ - dstd, d_ + dstd, alpha=0.5, facecolor='grey')
+        plt.legend()
+        plt.show()
 
-    data = pandas.read_csv('./Data/maha_data.csv')
-    firstDeathIdx = data[data['Total Deaths'] > 0].index[0]
-    deaths = data['New Deaths'][firstDeathIdx:].to_numpy()
-    days = deaths.size
+    names = ['S', 'E', 'A', 'I', 'Xs', 'Xe', 'Xa', 'Xi', 'P', 'R']
+    T = 40
+    N   = 1.1e8
+    E0, A0, I0 = 28, 238, 1
+    init = np.array([N - E0 - A0 - I0, E0, A0, I0, 0, 0, 0, 0, 0, 0])
+
+    E0_, A0_, I0_ = 50, 200, 1
+    init_ = np.array([N - E0_ - A0_ - I0_, E0, A0_, I0_, 0, 0, 0, 0, 0, 0])
+
     model = getModel()
-    R = 1
-    P0 = np.diag([100, 100, 100, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3])
-    H = np.array([[0, 1, 1, 1, 1, 1, 1, 0]]) * 0.02
-    active = data['Total Cases'] - data['Total Recoveries'] - data['Total Deaths']
-    active = active.to_numpy()
-    xs = extendedKalmanFilter(model.timeUpdate, init, P0, H, R, deaths, days)
-    predictedNewDeaths = xs @ H.T
-    print (xs)
-    plt.scatter(range(17, 17 + days), deaths, c='violet', label='new deaths (mar 17 onwards)')
-    plt.plot(range(17, 17 + days), predictedNewDeaths, c='red', label='predicted new deaths (mar 17 onwards)')
-    plt.plot(range(days), xs[:,2] + xs[:,5], c='blue', label='predicted infected')
-    plt.plot(range(days), xs[:,1] + xs[:,4], c='yellow', label='predicted asymptomatic')
-    plt.plot(range(14, 14 + active.size), active, c='green', label='active cases')
 
-    plt.legend()
-    plt.show()
+    xs = simulator(model, init, np.arange(0, T))[:-17]
+    print(xs[:,0])
+    print(xs[:,1])
+
+    deaths = (xs[:,3] + xs[:,-2]) * 0.02
+    days = deaths.size
+
+    R = 5
+    P0 = np.diag([150**2, 50**2, 50**2, 50**2, 1, 1, 1, 1, 1, 1])
+    H = np.array([[0, 0, 0, 1, 0, 0, 0, 0, 1, 0]]) * 0.02
+    xs_, Ps_ = extendedKalmanFilter(model.timeUpdate, init_, P0, H, R, deaths, days)
+
+    # pltColumn(2)
--- Simulate.py
+++ Simulate.py
@@ -1,8 +1,7 @@
 from Util import *
 from scipy.integrate import odeint
 
-def simulator (model, T) :
-    x0 = model.init
+def simulator (model, x0, T) :
     dx = model.dx
     with stdout_redirected() : 
         result = odeint(dx, x0, T)
