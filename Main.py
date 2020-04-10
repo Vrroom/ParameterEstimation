4e341739ce766211b7ad8e82f39669f07945f7d4
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2020-04-10 18:35:43 +0530

    Date Facility

--- Main.py
+++ Main.py
@@ -26,35 +26,39 @@ def getModel () :
 if __name__ == "__main__" : 
     T = 70
     N   = 1e8
-    A0, I0 = 1, 1
+    A0, I0 = 20, 20
     init = np.array([N - A0 - I0, A0, I0, 0, 0, 0, 0, 0])
 
-    A0_, I0_ = 50, 20
+    A0_, I0_ = 25, 25
     init_ = np.array([N - A0_ - I0_, A0_, I0_, 0, 0, 0, 0, 0])
     print(init, init_)
 
     model = getModel()
 
-    xs = simulator(model, init, np.arange(T))[:-17]
-
-    deaths = (xs[:,2] + xs[:,-2]) * 0.02
+    data = pandas.read_csv('./Data/maha_data7apr.csv')
+    totalDeaths = data['Total Deaths'].to_numpy()
+    deaths = data['New Deaths'].to_numpy()
+    P = (data['Total Cases'] - data['Total Recoveries'] - data['Total Deaths']).to_numpy()
+    deaths = deaths[totalDeaths > 0] 
     days = deaths.size
+    zs = np.stack([deaths, P[:]])
+    import pdb 
+    pdb.set_trace()
 
-    R = 50
-    P0 = np.diag([1e4, 1e4, 1e4, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3])
-    H = np.array([[0, 0, 1, 0, 0, 0, 1, 0]]) * 0.02
+    xs = simulator(model, init, np.arange(T))[:days]
+
+    R = 5
+    P0 = np.diag([1e2, 1e2, 1e2, 1e2, 1e2, 1e2, 1e2, 1e2])
+    H = np.array([[0, 0, .02, 0, 0, 0, 0.02, 0], [0, 0, 0, 0, 0, 0, 1, 0]])
     xs_, Ps_ = extendedKalmanFilter(model.timeUpdate, init_, P0, H, R, deaths, days)
-    print(xs_[0])
-
 
     istd = np.sqrt(np.array([np.diag(P)[-2] for P in Ps_]))
     i   = np.array([x[-2] for x in xs])
     i_  = np.array([x[-2] for x in xs_])
     
-    plt.plot(np.arange(days), i_ - istd, c='orange', label='Lower Bound on I')
-    plt.plot(np.arange(days), i_ + istd, c='red', label='Upper Bound on I')
-    plt.plot(np.arange(days), i, c='blue', label='True I')
-    plt.plot(np.arange(days), i_, c='green', label='Estimate I')
+    plt.plot(np.arange(days), i, c='red', label='True P')
+    plt.plot(np.arange(days), i_, c='blue', label='Estimate P')
+    plt.fill_between(np.arange(days), i_ - istd, i_ + istd, facecolor="grey", alpha=0.5)
     plt.legend()
     plt.show()
 
